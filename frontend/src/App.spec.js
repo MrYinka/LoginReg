0fde34f8e8daef1ef2eb5a003d83f306a548582f
Yinka Robert <yinkakinbolajo@gmail.com>
2021-03-25 03:35:11 +0200

    Update App.spec.js

--- frontend/src/App.spec.js
+++ frontend/src/App.spec.js
@@ -1,1 +1,15 @@
 //Functional Testing Coming up!
+
+import React from "react";
+import { render, screen, fireEvent } from '@testing-library/react';
+
+import Routes from "./Routes";
+
+
+test('Home Page with Default Messages', () => {
+    const { getByText, getByLabelText } = render(<Routes/>);
+
+    getByText("Social Profile");
+    getByText("Simple Login/Register Application with PERN");
+
+});
